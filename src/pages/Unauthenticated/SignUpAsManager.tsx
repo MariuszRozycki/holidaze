6ae2c8991fc5421d0e406be0f1c031257acf41c9
MariuszRozycki <mariansc83@icloud.com>
2025-01-17 11:33:42 +0100

    Add Form to SignUpAsManager

--- src/pages/Unauthenticated/SignUpAsManager.tsx
+++ src/pages/Unauthenticated/SignUpAsManager.tsx
@@ -1,5 +1,5 @@
 import { useNavigateToElement } from "../../hooks";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 import { HeadingH1, GoBackButton, SignUpButton } from "../../components";
 
 const SignUpAsManager = () => {
@@ -10,7 +10,18 @@ const SignUpAsManager = () => {
     <Container>
       <GoBackButton />
       <HeadingH1>Sign As a Manager</HeadingH1>
-      <SignUpButton btnText='Sign up!' onClick={handleNavigate} />
+      <Form>
+        <Form.Group className='mb-3' controlId='formBasicName'>
+          <Form.Control type='text' placeholder='Enter name' />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Control type='email' placeholder='Enter email' />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <SignUpButton btnText='Sign up!' variant='primary' type='submit' onClick={handleNavigate} />
+      </Form>
     </Container>
   );
 };
