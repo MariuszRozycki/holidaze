3e417379e18808a85f251c467e432978aa3ea7e5
MariuszRozycki <mariansc83@icloud.com>
2025-02-17 15:49:46 +0100

    Add style changes to logged-user-component

--- src/components/Header/HeaderMainLayout.tsx
+++ src/components/Header/HeaderMainLayout.tsx
@@ -1,9 +1,10 @@
 import { forwardRef } from "react";
 import NavMainLayout from "../Nav/NavMainLayout";
+import "./Header.scss";
 
 const HeaderMainLayout = forwardRef<HTMLDivElement>((_, headerRef) => {
   return (
-    <header className='header position-sticky mb-3 top-0 z-3'>
+    <header className='header position-sticky mb-3 top-0'>
       <NavMainLayout ref={headerRef} />
     </header>
   );
--- src/components/LoggedUserComponent/LoggedUserComponent.tsx
+++ src/components/LoggedUserComponent/LoggedUserComponent.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAppContext } from "../../context/app/useAppContext";
-import { Container, Image } from "react-bootstrap";
+import { Dropdown, Image, Container } from "react-bootstrap";
 import "./LoggedUserComponent.scss";
 
 const LoggedUserComponent = () => {
@@ -14,25 +15,73 @@ const LoggedUserComponent = () => {
 
   const isCurrentPath = location.pathname === `/holidaze/user/logged-user-by-name/${userName}`;
 
-  const handleNavigate = () => {
+  const handleProfileClick = () => {
     navigate(`/holidaze/user/logged-user-by-name/${userName}`);
   };
 
+  const handleSettingsClick = () => {
+    navigate("/holidaze/user/logged-user-by-name/${userName}");
+  };
+
+  const handleLogout = () => {
+    navigate("/holidaze/");
+  };
+
   return (
-    <Container>
-      <div
-        className={`user-component-wrapper ${isCurrentPath ? "inactive" : ""}`}
-        onClick={isCurrentPath ? undefined : handleNavigate}
-      >
-        <button className='user-component-name fs-5'>
-          View your profile, <span className='fw-semibold'>{formattedUserName}</span>
-        </button>
-        <div className='logged-user-avatar-wrapper'>
-          <Image className='logged-user-avatar' src={userImage} />
-        </div>
-      </div>
+    <Container className={`${isCurrentPath ? "inactive" : ""}`}>
+      <Dropdown className='logged-user-component' align='end'>
+        <Dropdown.Toggle variant='link' className='p-0'>
+          <Image className='logged-user-avatar' src={userImage} alt='User Avatar' roundedCircle />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={handleProfileClick}>View Profile</Dropdown.Item>
+          <Dropdown.Item onClick={handleSettingsClick}>Settings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </Container>
   );
 };
 
 export default LoggedUserComponent;
+
+// import { useNavigate, useLocation } from "react-router-dom";
+// import { useAppContext } from "../../context/app/useAppContext";
+// import { Container, Dropdown, Image } from "react-bootstrap";
+// import "./LoggedUserComponent.scss";
+
+// const LoggedUserComponent = () => {
+//   const { state } = useAppContext();
+//   const userName = state.userProfile?.name;
+//   const formattedUserName = userName ? userName.charAt(0).toUpperCase() + userName.slice(1) : "";
+
+//   const userImage = state.userProfile?.avatar.url;
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//   const isCurrentPath = location.pathname === `/holidaze/user/logged-user-by-name/${userName}`;
+
+//   const handleNavigate = () => {
+//     navigate(`/holidaze/user/logged-user-by-name/${userName}`);
+//   };
+
+//   return (
+//     <Container>
+//       <div
+//         className={`user-component-wrapper ${isCurrentPath ? "inactive" : ""}`}
+//         onClick={isCurrentPath ? undefined : handleNavigate}
+//       >
+//         <button className='user-component-name fs-5'>
+//           View your profile, <span className='fw-semibold'>{formattedUserName}</span>
+//         </button>
+//         <div className='logged-user-avatar-wrapper'>
+//           <Image className='logged-user-avatar' src={userImage} />
+//         </div>
+//       </div>
+//     </Container>
+//   );
+// };
+
+// export default LoggedUserComponent;
